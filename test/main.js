ceacc7c3e551583a839c785ea4f3a36a4aaa0002
Andrew Osheroff <andrewosh@gmail.com>
2015-11-26 18:23:59 -0500

    Starting server before test suite begins

--- test/main.js
+++ test/main.js
@@ -1,3 +1,18 @@
+var forever = require('forever-monitor')
+
+var server = null
+
+before(function () {
+  console.log('starting server')
+  server = new (forever.Monitor)('start.js')
+  server.start()
+})
+
+after(function () {
+  console.log('stopping server')
+  server.stop()
+})
+
 setTimeout(function () {
   run()
 }, 2000)
