d0584bcc1b1e024e18c2d80e8ed968c2db74e8a0
Andrew Osheroff <andrewosh@gmail.com>
2015-11-30 14:12:19 -0500

    Figured out async test problem

--- test/main.js
+++ test/main.js
@@ -2,21 +2,20 @@ var forever = require('forever-monitor')
 
 var kubeClient = require('../lib/client.js')
 
+var local = false
 var server = null
 
 // the proxy server must be started before all else
 console.log('starting server')
-server = new (forever.Monitor)('start.js')
-server.start()
-
-after(function (done) {
-  console.log('stopping server')
-  server.on('stop', function () {
-    done()
-  })
-  server.stop()
+server = new (forever.Monitor)('start.js', {
+  max: 2
 })
+server.on('exit', function () {
+  local = true
+  console.log('Setting local to true')
+})
+server.start()
 
 setTimeout(function () {
   run()
-}, 2000)
+}, 5000)
