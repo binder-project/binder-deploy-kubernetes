f6f3667b19bc9d780bc92ae9f80f7e03ab955547
Andrew Osheroff <andrewosh@gmail.com>
2016-03-22 10:09:52 +0000

    bug fixes, memory leak fix, and preloading additions

--- scripts/daemon.js
+++ scripts/daemon.js
@@ -20,34 +20,36 @@ setInterval(function () {
   getDatabase(function (err, db) {
     if (err) logger.error('could not cleanup apps: {0}'.format(err))
     App.initialize(db)
-    App.find({}, function (err, apps) {
-      var ids = _.map(apps, function (app) { return app.id })
-      console.log('found the following apps: {0}'.format(JSON.stringify(ids)))
+    App.find({ state: 'deployed' }, function (err, apps) {
       if (err) return logger.error('could not cleanup apps: {0}'.format(err))
+      var ids = _.map(apps, function (app) { return app.id })
       var proxy = proxyClient(settings)
       proxy.getRoutes({ age: age }, function (err, routes) {
         console.log('found the following routes: ' + JSON.stringify(routes))
         if (err) return logger.error('could not cleanup apps: {0}'.format(err))
-        async.each(apps, function (app, next) {
-          if (app.location in routes) {
+        async.eachLimit(apps, 5, function (app, next) {
+          if (app._location() in routes) {
+            console.log('****************')
             console.log('app is inactive: {0}'.format(JSON.stringify(app.id)))
+            console.log('inactiveTime: {0}, deployTime: {1}, cullTimeout: {2}'.format(app.inactiveTime, app.deployTime, app.cullTimeout))
             app.inactiveTime += period
-            var cullTimeout = app.cullTimeout
-            if (app.deployTime && app.inactiveTime > cullTimeout) {
+            if (app.deployTime && app.inactiveTime > app.cullTimeout) {
               logger.info('culling app: {0}'.format(app.id))
-              app.remove(function (err) {
+              app._delete(function (err) {
                 return next(err)
               })
             } else {
               console.log('app was inactive, but not removing: {0}'.format(app.id))
               app._save(function (err) {
                 if (err) return logger.error('could not save updated inactive app: {0}'.format(err))
+                return next(err)
               })
             }
           } else {
             app.inactiveTime = 0
             app._save(function (err) {
               if (err) return logger.error('could not save updated active app: {0}'.format(err))
+              return next(err)
             })
           }
         }, function (err) {
